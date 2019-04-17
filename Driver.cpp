2e841244e38c119451ea662f0d0433cb1f5e7d6f
lukeingalls <45518011+lukeingalls@users.noreply.github.com>
2019-04-16 20:17:13 -0600

    A basic driver. Doesn't quite work yet.
    Read comments.

--- Driver.cpp
+++ Driver.cpp
@@ -1,3 +1,14 @@
 #include "Player.hpp"
 
-int main() {}
+int main() {
+	//Eventually this should become a menu like we've seen in other assignments. For now we need to get stuff working.
+	percentile_scoring ps;
+	ps.readinfile("CU.txt");
+	ps.sort_basic_arrays();
+	ps.print_top_n_points(3);
+	/*So this doesn't work. Output should be:
+	1. Tyler Bey		13.6
+	2. Mckinley Wright	13.0
+	3. Shane Gatling	9.7
+	*/
+}
