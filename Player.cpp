8c09cf503c6dc7ef2fbc321dfa91a5ddc82614af
lukeingalls <45518011+lukeingalls@users.noreply.github.com>
2019-04-16 18:27:26 -0600

    Adding players now availible

--- Player.cpp
+++ Player.cpp
@@ -11,6 +11,11 @@ void array_double(player arr[], int &cap) {
 	cap = new_cap; //Adjust the size of the array
 }
 
+bool is_num(std::string s) {
+	for (int i = 0; i < s.length(); i++) if (i != '1' && i != '2' i != '3' && i != '4' && i != '5' i != '6' && i != '7' && i != '8' i != '9' && i != '0' && i != '.') return 0;
+		return 1;
+}
+
 /*This is the exact same as before just using type std::string for the array*/
 // void array_double(std::string arr[], int cap) {
 // 	int new_cap = 2*cap; //Define what the new size of the array should be
@@ -193,4 +198,110 @@ void maxheap::repair_down(int nodeIndex) {
 		swap(&heap[compar], &heap[nodeIndex]);
 		repair_down(compar);
 	}
+}
+
+//
+//
+//
+// Percentile code below ⬇
+//
+//
+//
+
+percentile_scoring::percentile_scoring() {
+	size = 10;
+	ppg = new player*[size]; 
+	rpg = new player*[size];
+	apg = new player*[size];
+	spg = new player*[size];
+	bpg = new player*[size];
+	cumulative = new player*[size];
+
+	for (int i = 0; i < sizeof(weights)/sizeof(weights[0]); i++) {
+		weights[i] = 1;
+	} 
+
+	players = 0;
+}
+
+percentile_scoring::loadPlayer() {
+	std::string inputs[8];
+	team *t;
+	while (1) {
+		std::cout << "The teams currently in the system are:" << std::endl;
+		for (int i = 0; i < teams.size(); i++) std::cout << teams[i].name << std::endl;
+		std::cout << "What team does he play for?" << std::endl;
+		getline(std::cin, inputs[0]);
+		for (int i = 0; i < teams.size(); i++) {
+			if (teams[i].name == inputs[0]) {
+				t = &teams[i];
+				break;
+			}
+		}
+		std::cout << "Would you like to add this team? YES input 1. NO input 2." << std::endl;
+		getline(std::cin, inputs[1]);
+		if (inputs[1][0] == '1') {
+			teams.push_back(inputs[0]);
+			t = &teams[teams.size() - 1];
+			break;
+		}
+	}
+	std::cout << "Player name:" << std::endl;
+	getline(std::cin, inputs[1]);
+	while(1) {
+		std::cout << "PPG? ";
+		getline(std::cin, inputs[2]);
+		std::cout << std::endl;
+		if (is_num(inputs[2]))
+			break;
+		else
+			std::cout << "Input a float..." << std::endl;
+	}
+	while(1) {
+		std::cout << "RPG? ";
+		getline(std::cin, inputs[3]);
+		std::cout << std::endl;
+		if (is_num(inputs[3]))
+			break;
+		else
+			std::cout << "Input a float..." << std::endl;
+	}
+	while(1) {
+		std::cout << "APG? ";
+		getline(std::cin, inputs[4]);
+		std::cout << std::endl;
+		if (is_num(inputs[4]))
+			break;
+		else
+			std::cout << "Input a float..." << std::endl;
+	}
+	while(1) {
+		std::cout << "BPG? ";
+		getline(std::cin, inputs[5]);
+		std::cout << std::endl;
+		if (is_num(inputs[5]))
+			break;
+		else
+			std::cout << "Input a float..." << std::endl;
+	}
+	while(1) {
+		std::cout << "SPG? ";
+		getline(std::cin, inputs[6]);
+		std::cout << std::endl;
+		if (is_num(inputs[6]))
+			break;
+		else
+			std::cout << "Input a float..." << std::endl;
+	}
+	while (1) {
+		std::cout << "POS? ";
+		getline(std::cin, inputs[7]);
+		std::cout << std::endl;
+		if (inputs[7] == '1' || inputs[7] == '2' || inputs[7] == '3' || inputs[7] == '4' || inputs[7] == '5')
+			break;
+		else
+			std::cout << "Input an int..." << std::endl;
+	}
+	player p(inputs[1], stoi(inputs[7]), stof(inputs[2]), stof(inputs[3]), stof(inputs[4]), stof(inputs[5]), stof(inputs[6]));
+	t->roster.push_back(p);
 }
