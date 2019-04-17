0c77045719c7dee75d6bb931725e946a599e45db
lukeingalls <45518011+lukeingalls@users.noreply.github.com>
2019-04-17 17:22:46 -0600

    No significant change.

--- Driver.cpp
+++ Driver.cpp
@@ -8,7 +8,7 @@ int main() {
 	ps.print_all_players();
 	ps.sort_basic_arrays();
 	std::cout << std::endl;
-	ps.print_top_n_points(3);
+	ps.print_top_n_rebounds(4);
 	/*So this doesn't work. Output should be:
 	1. Tyler Bey		13.6
 	2. Mckinley Wright	13.0
