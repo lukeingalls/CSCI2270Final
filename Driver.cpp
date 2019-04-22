4fb035f751c505e57f6b37468e7526c82bb4362e
evanmeany <49416591+evanmeany@users.noreply.github.com>
2019-04-21 19:47:47 -0600

    Update Driver.cpp

--- Driver.cpp
+++ Driver.cpp
@@ -1,12 +1,6 @@
 #include "Player.hpp"
-#include <iostream>
 
 int main() {
-	//Eventually this should become a menu like we've seen in other assignments. For now we need to get stuff working.
 	percentile_scoring ps;
-	ps.readinfile("CU.txt");
-	ps.print_all_players();
-	ps.rank_top();
-	std::cout << std::endl;
-	ps.print_top_n_rebounds(4);
+	ps.readinfile("train_data.csv");
 }
